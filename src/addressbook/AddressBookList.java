9f880c2587475d6b11768d4413190e1a05808261
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-15 20:52:50 +0530

    [Prajakta]: Added new class for added multiple address book

--- src/addressbook/AddressBookList.java
+++ src/addressbook/AddressBookList.java
@@ -1,2 +1,23 @@
-package addressbook;public class AddressBookList {
+package addressbook;
+
+public class AddressBookList
+ {
+   public String userInputBookName;
+
+   public AddressBookList(String userInputBookName)
+      {
+        this.userInputBookName = userInputBookName;
+      }
+   public String getUserInputBookName()
+     {
+        return  userInputBookName;
+     }
+   public void setUserInputBookName(String firstName)
+     {
+        this.userInputBookName = userInputBookName;
+     }
+   public String toString()
+     {
+        return "AddressBookName:" + userInputBookName + "\n";
+     }
 }
